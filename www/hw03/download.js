73c7ad9c2c5f8ca21b9a52482596a57774996fad
Viet Bach Nguyen <nvbach91@outlook.com>
2023-10-12 22:44:41 +0200

    added hw03 pre

---
+++ www/hw03/download.js
@@ -0,0 +1,146 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const bluebird = require('bluebird');
+
+const ensureDirectoryExistence = (filePath) => {
+    var dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+};
+
+const urls = [
+    'https://eso.vse.cz/~pank09/cv03/homework/',
+    'https://eso.vse.cz/~pank09/cv03/blog/',
+    'https://eso.vse.cz/~baia04/cv03/homework/',
+    'https://eso.vse.cz/~baia04/cv03/blog/',
+    'https://eso.vse.cz/~egea00/cv03/homework/',
+    'https://eso.vse.cz/~egea00/cv03/blog/',
+    'https://eso.vse.cz/~mati04/cv03/homework/',
+    'https://eso.vse.cz/~mati04/cv03/blog/',
+    'https://eso.vse.cz/~hujl00/cv03/blog/',
+    'https://eso.vse.cz/~hujl00/cv03/homework/',
+    'https://eso.vse.cz/~heno00/cv03/homework/',
+    'https://eso.vse.cz/~heno00/cv03/blog/',
+    'https://eso.vse.cz/~peta25/cv03/homework/',
+    'https://eso.vse.cz/~peta25/cv03/blog/',
+    'https://eso.vse.cz/~juha00/cv03/homework/',
+    'https://eso.vse.cz/~juha00/cv03/blog/',
+    'https://eso.vse.cz/~brud06/cv03/homework/',
+    'https://eso.vse.cz/~brud06/cv03/blog/',
+    'https://eso.vse.cz/~kadm09/cv03/homework/',
+    'https://eso.vse.cz/~kadm09/cv03/blog/',
+    'https://eso.vse.cz/~knyd00/cv03/homework/',
+    'https://eso.vse.cz/~knyd00/cv03/blog/',
+    'https://eso.vse.cz/~mata30/cv03/homework/',
+    'https://eso.vse.cz/~mata30/cv03/blog/',
+    'https://eso.vse.cz/~sime06/cv03/homework/',
+    'https://eso.vse.cz/~sime06/cv03/blog/',
+    'https://eso.vse.cz/~kask05/cv03/blog/',
+    'https://eso.vse.cz/~kask05/cv03/homework/',
+    'https://eso.vse.cz/~zolf00/cv03/homework/',
+    'https://eso.vse.cz/~zolf00/cv03/blog/',
+    'https://eso.vse.cz/~lubv00/cv03/homework/',
+    'https://eso.vse.cz/~lubv00/cv03/blog/',
+    'https://eso.vse.cz/~talp03/cv03/homework/',
+    'https://eso.vse.cz/~talp03/cv03/blog/',
+    'https://eso.vse.cz/~shei03/cv03/blog/',
+    'https://eso.vse.cz/~shei03/cv03/homework/',
+    'https://eso.vse.cz/~lanj21/cv03/homework/',
+    'https://eso.vse.cz/~lanj21/cv03/blog/',
+    'https://eso.vse.cz/~kvaj02/cv03/homework/',
+    'https://eso.vse.cz/~kvaj02/cv03/blog/',
+    'https://eso.vse.cz/~hrak04/cv03/homework/',
+    'https://eso.vse.cz/~hrak04/cv03/blog/',
+    'https://eso.vse.cz/~tomo03/cv03/homework/',
+    'https://eso.vse.cz/~tomo03/cv03/blog/',
+    'https://eso.vse.cz/~sykv01/cv03/homework/',
+    'https://eso.vse.cz/~sykv01/cv03/blog/',
+    'https://eso.vse.cz/~smem09/cv03/homework/',
+    'https://eso.vse.cz/~smem09/cv03/blog/',
+    'https://eso.vse.cz/~ruzo03/cv03/Homework/',
+    'https://eso.vse.cz/~ruzo03/cv03/Blog/',
+    'https://eso.vse.cz/~stra24/cv03/homework/',
+    'https://eso.vse.cz/~stra24/cv03/blog/',
+    'https://eso.vse.cz/~elcs00/cv03/homework/',
+    'https://eso.vse.cz/~elcs00/cv03/blog/',
+    'https://eso.vse.cz/~lanm16/cv03/homework/',
+    'https://eso.vse.cz/~lanm16/cv03/blog/',
+    'https://eso.vse.cz/~mala24/cv03/homework/',
+    'https://eso.vse.cz/~mala24/cv03/blog/',
+    'https://eso.vse.cz/~urbf01/cv03/homework/',
+    'https://eso.vse.cz/~urbf01/cv03/blog/',
+    'https://eso.vse.cz/~capm03/cv03/homework/',
+    'https://eso.vse.cz/~capm03/cv03/blog/',
+    'https://eso.vse.cz/~shla01/cv03/homework/',
+    'https://eso.vse.cz/~shla01/cv03/blog/',
+    'https://eso.vse.cz/~kadv05/cv03/homework/',
+    'https://eso.vse.cz/~kadv05/cv03/blog/',
+    'https://eso.vse.cz/~kudj05/cv03/homework/',
+    'https://eso.vse.cz/~kudj05/cv03/blog/',
+    'https://eso.vse.cz/~stik05/cv03/homework/',
+    'https://eso.vse.cz/~stik05/cv03/blog/',
+    'https://eso.vse.cz/~draj08/cv03/homework/',
+    'https://eso.vse.cz/~draj08/cv03/blog/',
+    'https://eso.vse.cz/~lism08/cv03/homework/',
+    'https://eso.vse.cz/~lism08/cv03/blog/',
+    'https://eso.vse.cz/~binj02/cv03/homework/',
+    'https://eso.vse.cz/~binj02/cv03/blog/',
+    'https://eso.vse.cz/~vanm34/cv03/homework/',
+    'https://eso.vse.cz/~vanm34/cv03/blog/',
+    'https://eso.vse.cz/~pavj16/cv03/homework/',
+    'https://eso.vse.cz/~pavj16/cv03/blog/',
+    'https://eso.vse.cz/~mald10/cv03/homework/',
+    'https://eso.vse.cz/~mald10/cv03/blog/',
+    'https://eso.vse.cz/~kire02/cv03/homework/',
+    'https://eso.vse.cz/~kire02/cv03/blog/',
+    'https://eso.vse.cz/~jure01/cv03/homework/',
+    'https://eso.vse.cz/~jure01/cv03/blog/',
+    'https://eso.vse.cz/~hore06/cv03/homework/',
+    'https://eso.vse.cz/~hore06/cv03/blog/',
+    'https://eso.vse.cz/~vlcj07/cv03/blog/',
+    'https://eso.vse.cz/~vlcj07/cv03/homework/',
+    'https://eso.vse.cz/~kadf01/cv03/homework/',
+    'https://eso.vse.cz/~kadf01/cv03/blog/',
+    'https://eso.vse.cz/~svaa03/cv03/homework/',
+    'https://eso.vse.cz/~malj17/cv03/homework/',
+    'https://eso.vse.cz/~malj17/cv03/blog/',
+    'https://eso.vse.cz/~zabv04/cv03/homework/',
+    'https://eso.vse.cz/~zabv04/cv03/blog/',
+    'https://eso.vse.cz/~mact13/cv03/homework/',
+    'https://eso.vse.cz/~mact13/cv03/blog/',
+    'https://eso.vse.cz/~telo00/cv03/homework/',
+    'https://eso.vse.cz/~telo00/cv03/blog/',
+    'https://eso.vse.cz/~voze00/cv03/homework/',
+    'https://eso.vse.cz/~voze00/cv03/blog/',
+
+    // 'https://eso.vse.cz/~trah09/homework/',
+    // 'https://eso.vse.cz/~trah09/blog/',
+    // 'https://eso.vse.cz/~janj56/du2/homework/',
+    // 'https://eso.vse.cz/~janj56/du2/blog/',
+    // 'https://eso.vse.cz/~kond12/cv03.1/',
+    // 'https://eso.vse.cz/~kond12/blog/',
+    // 'https://eso.vse.cz/~auza00/cv_03/homework/',
+    // 'https://eso.vse.cz/~auza00/cv_03/blog/',
+    // 'https://eso.vse.cz/~bobd03/DU3/',
+    // 'https://eso.vse.cz/~bobd03/DU3/blog/',
+].map((url) => {
+        if (url.endsWith('.html')) {
+            return url;
+        }
+        return `${url.endsWith('/') ? url : `${url}/`}index.html`;
+    });
+
+bluebird.each(urls, (url) => {
+    return axios.get(url).then((resp) => {
+        const filePath = `./${url.split('/').slice(3).join('/').slice(1).replace('cv03/', '')}`;
+        ensureDirectoryExistence(filePath);
+        fs.writeFileSync(filePath, resp.data);
+    }).catch((err) => {
+        console.log(url);
+    });
+});
+
