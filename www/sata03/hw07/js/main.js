b6bb14c920a49608d883e55772fd3f285f711600
satekova <145775323+satekova@users.noreply.github.com>
2023-11-23 14:11:43 +0100

    sata07-hw07

---
+++ www/sata03/hw07/js/main.js
@@ -0,0 +1,65 @@
+const decipherText = () => {
+    const cipherText = document.getElementById('cipherText').value;
+    const key = parseInt(document.getElementById('key').value);
+
+    const decryptedText = caesarDecipher(cipherText, key);
+
+    document.getElementById('decryptedText').innerText = `Decrypted Text: ${decryptedText}`;
+};
+
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const shiftChar = (c, shift) => {
+    // a helper function to shift one character inside the 
+    // alphabet based on the shift value and return the result
+    const index = alphabet.indexOf(c);
+    if (index === -1) {
+        // character not found in the alphabet, return it unchanged
+        return c;
+    }
+    // shift the character and handle wrapping around the alphabet
+    return alphabet[(index - shift + 26) % 26];
+
+};
+
+const shiftString = (str, shift) => {
+    // a helper function to shift one entire string inside the 
+    // alphabet based on the shift value and return the result
+    return str
+        .split('')
+        .map((char) => shiftChar(char, shift))
+        .join('');
+};
+
+const caesarDecipher = (cipherText, usedKey) => {
+    // your implementation goes here
+    // good to know: 
+    //    str.indexOf(c) - returns the index of the specified character in the string
+    //    str.charAt(i) - returns the character at the specified index in the string
+    //    when the shifted character is out of bound, it goes back to the beginning and count on from there
+    // convert the cipher text to uppercase for consistency
+    cipherText = cipherText.toUpperCase();
+    // decipher each character in the cipher text
+    return shiftString(cipherText, usedKey);
+
+};
+
+// albert einstein
+console.log(
+    caesarDecipher(
+        "MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG", 19)
+);
+
+// john archibald wheeler
+console.log(
+    caesarDecipher("YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW", 5)
+);
+
+// charles darwin
+console.log(
+    caesarDecipher("M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ", 12)
+);
+
+console.log(
+    caesarDecipher("M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ", 12)
+);
