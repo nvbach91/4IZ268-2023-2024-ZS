12128af6cf3ce7021b5690487e13eb40c4241867
Aleksandra Matveeva <145464206+AleksandraMatveeva@users.noreply.github.com>
2023-12-17 15:43:16 +0100

    mata30-sp2
    Pilotní verze: https://eso.vse.cz/~mata30/sp2/

---
+++ www/mata30/sp2/main.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('.container');
+const searchButton = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
