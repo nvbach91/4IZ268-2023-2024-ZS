bc56ea2fae384ca30989e71a9c82b4d54555b4cf
LBurn4 <115028418+LBurn4@users.noreply.github.com>
2023-12-17 18:58:57 +0100

    burl09-sp2
    Link to application: https://eso.vse.cz/~burl09/sp2/

---
+++ www/burl09/sp2/js/main.js
@@ -0,0 +1,8 @@
+let move_speed = 2; 
+     
+let gravity = 1; 
+    
+let bird = document.querySelector('.bird'); 
+     
+let bird_props = bird.getBoundingClientRect(); 
+let background = document.querySelector('.background').getBoundingClientRect();
