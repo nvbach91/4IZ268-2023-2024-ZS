e3dff2d4dd4a124f349cb20fdee7dc427f825400
Viet Bach Nguyen <nvbach91@outlook.com>
2024-01-12 18:13:22 +0100

    added sp1-new

---
+++ www/sp1/pist02/sp1-new/scripts/index.js
@@ -0,0 +1,9 @@
+/*document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const nav = document.getElementById("nav");
+
+  menuIcon.addEventListener("click", function () {
+    nav.classList.toggle("show-menu");
+  });
+});
+*/
