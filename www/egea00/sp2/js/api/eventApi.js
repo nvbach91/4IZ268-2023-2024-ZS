b496853ba222300c9880202e967a68f35eab2565
Adam <adacine123@gmail.com>
2024-01-18 13:43:28 +0100

    FINALNI verze - (w/o NODE proxy-server)
    nefunkcni API calls pro Weather & Events, nutno preroutovat API call na proxy-server

---
+++ www/egea00/sp2/js/api/eventApi.js
@@ -0,0 +1,30 @@
+const eventApiConfig = {
+    apiUrl: 'https://app.ticketmaster.com/discovery/v2/events.json?',
+    api3000Url: 'http://localhost:3000/ticketmaster/discovery/v2/events?',
+    apiKey: 'ury1bustrLLAnMLKbWaX1vkvLxWnxKfi'
+};
+
+function fetchEventsByCity(cityName, successCallback, errorCallback) {
+    const url = `${eventApiConfig.api3000Url}apikey=${eventApiConfig.apiKey}&locale=*&city=${cityName}`;
+
+    $.ajax({
+        method: 'GET',
+        url: url,
+        contentType: 'application/json',
+        success: function (data) {
+            if (data && data.page.totalElements === 0) {
+                errorCallback('404');
+            } else {
+                successCallback(data);
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            if (jqXHR.status) {
+                errorCallback(jqXHR.status.toString(), textStatus);
+            } else {
+                errorCallback('network', textStatus);
+            }
+        }
+    });
+}
+
