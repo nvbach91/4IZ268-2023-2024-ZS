b2e3ff51c890cd7b51d8b185a20f164e74bd8923
LucieH22 <145653530+LucieH22@users.noreply.github.com>
2024-01-18 23:27:37 +0100

    finální verze 02
    Opraveno vše. https://eso.vse.cz/~hujl00/sp2/

--- www/hujl00/sp2/js/main.js
+++ www/hujl00/sp2/js/main.js
@@ -1,7 +1,7 @@
 const createApp = () => {
     const main = $('<main>').addClass('main');
     const title = $('<div>').addClass('title');
-    title.text('Hangman: guess the word')
+    title.text('Hangman: guess the word');
     const header = $('<header>').addClass('header');
     const leftColumn = $('<div>').addClass('left-column');
     const startButton = $('<button>').addClass('start-button');
@@ -12,7 +12,8 @@ const createApp = () => {
         hints.empty();
         end.empty();
         showLoading();
-        startGame();
+        getWord();
+        gameWon = false;
         $('.unclickable').removeClass('unclickable').addClass('key');
         gamesPlayed = gamesPlayed + 1;
         countGames.text('Games played: ' + gamesPlayed);
@@ -26,7 +27,30 @@ const createApp = () => {
     const showRulesButton = $('<button>').addClass('show-rules-button');
     showRulesButton.attr('id', 'show-rules');
     showRulesButton.text('Game rules');
-
+    const guessedWordsDictionary = {};
+    const addWordToDictionary = (word, definition) => {
+            guessedWordsDictionary[word] = definition;
+        };
+    const showDictionaryButton = $('<button>').addClass('dictionary-button');
+    showDictionaryButton.text('Show dictionary of guessed words');
+    const dictionary = $('<div>').addClass('dictionary');
+    const unshowDictionaryButton = $('<button>').addClass('dictionary-button');
+    unshowDictionaryButton.text('Close dictionary of guessed words');
+    const wordsInDictionary = $('<div>').addClass('words-dictionary');
+    dictionary.append(wordsInDictionary, unshowDictionaryButton);
+    showDictionaryButton.click(function () {
+        wordsInDictionary.empty();
+        wordsInDictionary.append($('<h2>').text('Guessed Words Dictionary'));
+        for (const [word, definition] of Object.entries(guessedWordsDictionary)) {
+            const guessedWordLine = $('<div>').text(`${word}: ${definition}`);
+            wordsInDictionary.append(guessedWordLine);
+        } 
+        dictionary.fadeIn();
+        });
+    unshowDictionaryButton.click(function () {
+        dictionary.fadeOut();
+    });
+    dictionary.fadeOut();
     const rules = $('<div>').addClass('rules');
     rules.text('Game rules: You start by clicking on the "Start new game" button. Now at least four lines appear on your screen. The number of lines represent the number of letters in a random english word. Your task is to guess which letters are represented by the lines. By clicking on the letters bellow you can reveal the letter if your guess is correct. However; if you guess too many letters wrong, you lose the game. Good luck, have fun!');
     rules.attr('id', 'rules');
@@ -34,16 +58,13 @@ const createApp = () => {
     unshowRulesButton.attr('id', 'unshow-rules');
     unshowRulesButton.text('Close');
     rules.append(unshowRulesButton);
-    $(document).ready(function () {
-        $('#show-rules').click(function () {
-            $('#rules').fadeIn();
+    showRulesButton.click(function () {
+            rules.fadeIn();
         });
-        $('#unshow-rules').click(function () {
-            $('#rules').fadeOut();
+    unshowRulesButton.click(function () {
+            rules.fadeOut();
         });
-    });
-
-    leftColumn.append(startButton, showRulesButton);
+    leftColumn.append(startButton, showRulesButton, showDictionaryButton);
     const showHint1 = $('<button>').addClass('show-hint-button');
     showHint1.text('Show first hint');
     const rightColumn = $('<div>').addClass('right-column');
@@ -70,28 +91,22 @@ const createApp = () => {
     const end = $('<div>').addClass('end');
 
     const keyboard = $('<div>').addClass('keyboard');
-    const keyboardFirstRow = $('<div>').addClass('keyboard-row');
-    const keyboardSecondRow = $('<div>').addClass('keyboard-row');
-    const keyboardThirdRow = $('<div>').addClass('keyboard-row');
-    const lettersFirstRow = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
-    const lettersSecondRow = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
-    const lettersThirdRow = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-    lettersFirstRow.forEach((letterK) => {
-        const key = $(`<div>${letterK}</div>`);
-        key.addClass('key');
-        keyboardFirstRow.append(key)
+    const letterRows = [
+        ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'], 
+        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'], 
+        ['Z', 'X', 'C', 'V', 'B', 'N', 'M'], 
+      ];
+      letterRows.forEach((row) => {
+        const keyboardRow = $('<div>').addClass('keyboard-row'); 
+    
+        row.forEach((letter) => {
+            const key = $('<div>').text(letter).addClass('key'); 
+            keyboardRow.append(key);
+        });
+    
+        keyboard.append(keyboardRow); 
     });
-    lettersSecondRow.forEach((letterK) => {
-        const key = $(`<div>${letterK}</div>`);
-        key.addClass('key');
-        keyboardSecondRow.append(key)
-    });
-    lettersThirdRow.forEach((letterK) => {
-        const key = $(`<div>${letterK}</div>`);
-        key.addClass('key');
-        keyboardThirdRow.append(key)
-    });
-    keyboard.append(keyboardFirstRow, keyboardSecondRow, keyboardThirdRow);
+    
 
     let points = parseInt(localStorage.getItem('hangmanPoints')) || 0;
     const counters = $('<div>').addClass('count-points');
@@ -112,38 +127,69 @@ const createApp = () => {
     const hideLoading = () => {
         spinner.remove();
     };
-    const startGame = () => {
-        const getWordsURL = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/111863/dictionary.txt';
-        $.get(getWordsURL, (data) => {
-            const words = data.split('\n');
-            const randomIndex = Math.floor(Math.random() * words.length);
-            const randomWord = words[randomIndex];
-            if (randomWord.length <= 3) {
+    const getWordsURL = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/111863/dictionary.txt';
+    const getWord = () => {$.get(getWordsURL, (data) => {
+        const words = data.split('\n');
+        startGame(words); 
+    });}
+    getWord();
+    gamesPlayed = gamesPlayed + 1;
+    countGames.text('Games played: ' + gamesPlayed);
+    localStorage.setItem('hangmanGamesPlayed', gamesPlayed);
+    localStorage.setItem('hangmanPoints', points);
+    numberOfTrials = 8;
+    updateTrialsColor();
+    trialsLeft.text('Number of trials left: ' + numberOfTrials);
+    localStorage.setItem('hangmanPoints', points);
+    
+    const startGame = (words) => {
+        const randomIndex = Math.floor(Math.random() * words.length);
+        const randomWord = words[randomIndex];
+        if (randomWord.length <= 3) {
                 startGame();
-            } else {
+        } else {
                 const letters = randomWord.split('');
                 letters.forEach((letter) => {
                     const line = $(`<div>${letter}</div>`);
                     line.addClass('line-unrevealed line');
                     spaceForWord.append(line)
-                })
+                });
                 rightColumn.append(showHint1);
+                showHint1.prop('disabled', false);
                 showHint1.on('click', () => {
                     showLoading();
                     hintN1(randomWord);
                     rightColumn.append(showHint2);
+                    showHint2.prop('disabled', false);
                     showHint2.on('click', () => {
                         showLoading();
                         hintN2(randomWord);
                     });
                 });
+                const settings = {
+                    async: true,
+                    crossDomain: true,
+                    url: 'https://wordsapiv1.p.rapidapi.com/words/' + randomWord + '/definitions',
+                    method: 'GET',
+                    headers: {
+                        'X-RapidAPI-Key': 'e8c33a1ec8msh2020509a6ffe124p105323jsnf081559fa303',
+                        'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+                    }
+                };
+                let definitions;
+                $.ajax(settings)
+                    .done(function (response) {
+                        if (response.definitions && response.definitions.length > 0) {
+                            definitions = response.definitions.map(def => def.definition);
+                            definitions = definitions.join('\n');
+                        } 
+                    })
                 keyboard.off('click', '.key').on('click', '.key', function () {
-                    keyClick($(this), randomWord);
+                    keyClick($(this), randomWord, definitions);
                 });
                 hideLoading();
             }
-        });
-    };
+        };
     const hintN1 = (randomWord) => {
         const settings = {
             async: true,
@@ -170,6 +216,7 @@ const createApp = () => {
                 hints.append(spaceForDefinition);
                 spaceForDefinition.text('Sorry, we could not find the definition.');
             });
+        showHint1.prop('disabled', true);
         hideLoading();
     }
     const hintN2 = (randomWord) => {
@@ -199,11 +246,13 @@ const createApp = () => {
                 hints.append(spaceForSynonyms);
                 spaceForSynonyms.text('Sorry, we could not find any synonyms.');
             });
+        showHint2.prop('disabled', true);
         hideLoading();
     }
-    const keyClick = (key, randomWord) => {
+    let gameWon = false;
+    const keyClick = (key, randomWord, definitions) => {
         const clickableKey = key.attr('class');
-        if (clickableKey === 'unclickable' || numberOfTrials === 0) {
+        if (clickableKey === 'unclickable' || numberOfTrials === 0 || gameWon === true) {
             return false;
         }
         key.removeClass('key').addClass('unclickable');
@@ -216,6 +265,8 @@ const createApp = () => {
                 $(this).removeClass('line-unrevealed').addClass('line-revealed');
                 let numberOfLettersRevealed = $('.line-revealed').length;
                 if (numberOfLetters === numberOfLettersRevealed) {
+                    gameWon = true;
+                    addWordToDictionary(randomWord, definitions);
                     end.text('You won!');
                     end.append(shareButton);
                     points = points + 1;
@@ -239,7 +290,7 @@ const createApp = () => {
             }
         }
     }
-    leftColumn.append(rules);
+    leftColumn.append(rules, dictionary);
     header.append(leftColumn, rightColumn);
     counters.append(countPoints, countGames);
     main.append(title, header, hints, trialsLeft, spaceForWord, keyboard, counters, end, footer);
