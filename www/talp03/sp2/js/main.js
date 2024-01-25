4d824e9876e7a5685ff5ba4a1304c8e08613d39c
Patrik <139178681+AsemikTEA@users.noreply.github.com>
2024-01-25 21:40:51 +0100

    Skoro finální verze
    - dodělání funkcionality vyhledání místa a trasy
    - přidání možnosti mezistanice
    - geolokace
    - kód je přehlednější

--- www/talp03/sp2/js/main.js
+++ www/talp03/sp2/js/main.js
@@ -1,236 +1,228 @@
-const appContainer = $('#app');
-const mapContainer = $('<div>', {id: 'map','class': 'map'});
-const mapSearch = $('<div>', {'class': 'search-wrapper'});
-
-appContainer.append(mapSearch);
-appContainer.append(mapContainer);
-
-const placeSearch = $('<div>', {'class': 'place-search'});
-const routeSearch = $('<div>', {'class': 'route-search'});
-
-mapSearch.append(placeSearch);
-mapSearch.append(routeSearch);
-
-const placeSearchBar = $('<input>', {id: 'p-search','class': 'p-search'});
-const originDestination = $('<input>', {id: 'origin','class': 'origin'});
-const finalDestination = $('<input>', {id: 'final','class': 'final'});
-const placeButton = $('<button>', {id: 'place-button','class': 'place-button'});
-const routeButton = $(`<button id='route-button' class='route-button'>Delete route</button>`);
-const modeSelector = $('<div>', {id: 'mode-selector','class': 'mode-selector'});
-const walking = $('<input>', {id: 'changemode-walking', name: 'type', type: 'radio', checked: 'checked', 'class': 'mode walking'});
-const driving = $('<input>', {id: 'changemode-driving', name: 'type', type: 'radio', checked: 'checked', 'class': 'mode driving'});
-const transit = $('<input>', {id: 'changemode-transit', name: 'type', type: 'radio', checked: 'checked', 'class': 'mode transit'});
-const labelWalking= $('<label>', {for: 'changemode-walking'}).text('Walking');
-const labelDriving = $('<label>', {for: 'changemode-driving'}).text('Driving');
-const labelTransit = $('<label>', {for: 'changemode-transit'}).text('Transit');
-
-placeSearch.append(placeSearchBar);
-placeSearch.append(placeButton);
-routeSearch.append(originDestination);
-routeSearch.append(finalDestination);
-routeSearch.append(routeButton);
-routeSearch.append(modeSelector);
-modeSelector.append(walking);
-modeSelector.append(driving);
-modeSelector.append(transit);
-modeSelector.append(labelWalking);
-modeSelector.append(labelDriving);
-modeSelector.append(labelTransit);
-
-let map;
-
-//routeButton.addEventListener("click", a);
-
-
 async function initMap() {
+  
   // The location of Uluru
   const position = { lat: -25.344, lng: 131.031 };
+  
   // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-
+  const { Map } = await google.maps.importLibrary('maps');
+  
   // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
+  let map = new Map($('#map')[0], {
     zoom: 4,
     center: position,
-    mapId: "DEMO_MAP_ID",
+    mapId: 'DEMO_MAP_ID',
   });
 
-  new SearchPlace(map)
-  new AutocompleteRouteHandler(map);
-};
+  //Classes from Google API
+  const marker = new google.maps.Marker();
+  const infoWindow = new google.maps.InfoWindow();
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  let travelMode = google.maps.TravelMode.DRIVING;
 
-// class handling search of individual place
-class SearchPlace {
-  map;
-  place;
-  placeLatLng;
-  marker;
-  //infoWindow;
-
-  constructor(map) {
-    this.map = map;
-    this.place = [];
-    this.placeLatLng = {};
-    this.marker = new google.maps.Marker();
-    this.infoWindow = new google.maps.InfoWindow();
-    
-    const autocompletePlaces = new google.maps.places.Autocomplete(
-      document.getElementById('p-search'),
-      { fields: ['geometry', 'formatted_address', 'name']},
-    );
-    autocompletePlaces.addListener('place_changed', () => {
-      this.place = autocompletePlaces.getPlace();
-      this.placeLatLng = {lat: this.place.geometry.location.lat(), lng: this.place.geometry.location.lng()};
-      this.setMarker();
-      this.setInfoWindow();
-      const placeInfo = this.place.formatted_address;
-      console.log(placeInfo);
-    });
-
-   //routeButton.addEventListener("click", a);
+  //Local variables
+  const originInput = $('#origin')[0];
+  const destinationInput = $('#final')[0];
+  let originPlace = '';
+  let finalPlace = '';
+  const waypointPlace = [];
+  
+  //Loading animation
+  const spinner = $('<div class="spinner"></div>');
+  
+  const showLoading = () => {
+    $('#route-search').append(spinner[0]);
+    console.log('cvbcc');
+  };
+  
+  const hideLoading = () => {
+    spinner.remove();
   };
 
-  setMarker() {
-    this.marker.setMap(null);
-    this.marker.setPosition(this.placeLatLng);
-    this.marker.setMap(this.map);
-    this.marker.setTitle(this.place.name);
-    this.map.setCenter(this.placeLatLng);
-    this.map.setZoom(12);
-  }
+  //Autocomplete handlers
+  const autocompletePlaces = new google.maps.places.Autocomplete(
+    $('#p-search')[0],
+    { fields: ['geometry', 'formatted_address', 'name']},
+  );
 
-  setInfoWindow() {
+  autocompletePlaces.addListener('place_changed', () => {
+    const place = autocompletePlaces.getPlace()
+    const placeLatLng = {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()};
+    setMarker(placeLatLng);
+    setInfoWindowPlace(place);
+  });
+
+  const originAutocomplete = new google.maps.places.Autocomplete(
+    originInput,
+    {fields: ['place_id']}
+  );
+  const finalAutocomplete = new google.maps.places.Autocomplete(
+    destinationInput,
+    {fields: ['place_id']}
+  );
+
+  originAutocomplete.addListener('place_changed', () => {
+    const place = originAutocomplete.getPlace();
+    originPlace = place.place_id;
+  });
+
+  finalAutocomplete.addListener('place_changed', () => {
+    const place = finalAutocomplete.getPlace();
+    finalPlace = place.place_id;
+    route();
+  });
+
+  //Functional methods for search of single place
+  const setInfoWindowPlace = (place) => {
     const infoContent = `<p>
-    Name: ${this.place.name}<br/>
-    Adress: ${this.place.formatted_address}
+    Name: ${place.name}<br/>
+    Adress: ${place.formatted_address}
     </p>`;
 
-    this.infoWindow.setContent(infoContent);
-    this.infoWindow.open({anchor: this.marker, map: map,});
+    infoWindow.setContent(infoContent);
+    infoWindow.open({anchor: marker, map: map,});
 
-    this.marker.addListener('click', () => {
-      this.infoWindow.open({anchor: this.marker, map: map,});
+    marker.addListener('click', () => {
+    infoWindow.open({anchor: marker, map: map,});
     })
   }
 
-};
+  const setMarker = (placeLatLng) => {
+    showLoading();
+    marker.setMap(null);
+    marker.setPosition(placeLatLng);
+    marker.setMap(map);
+    map.setCenter(placeLatLng);
+    map.setZoom(12);
+  }
+  
+  const geolocation = () => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const placeLatLng = {lat: position.coords.latitude, lng: position.coords.longitude};
+        setMarker(placeLatLng);
+      })
+    } else {
+      console.log('asdas');
+    }
+  }
 
-// class for handling search for route
-class AutocompleteRouteHandler {
-  map;
-  originPlace;
-  finalPlace;
-  directionsService;
-  directionsRenderer;
-  travelMode;
-  infoWindow;
-  travelTime;
-  travelDistance;
+  const clearPlace = () => {
+    marker.setMap(null);
+    infoWindow.setPosition(null);
+  }
 
-  constructor() {
-    this.map = map;
-    this.originPlace = '';
-    this.finalPlace = '';
-    this.travelTime = '';
-    this.travelDistance = '';
-    this.directionsService = new google.maps.DirectionsService();
-    this.directionsRenderer = new google.maps.DirectionsRenderer();
-    this.directionsRenderer.setMap(map);
-    this.travelMode = google.maps.TravelMode.DRIVING;
-    this.infoWindow = new google.maps.InfoWindow();
-
-    const originInput = document.getElementById("origin");
-    const destinationInput = document.getElementById("final");
-    
-    const originAutocomplete = new google.maps.places.Autocomplete(
-      originInput,
-      {fields: ['place_id']}
-    );
-    const finalAutocomplete = new google.maps.places.Autocomplete(
-      destinationInput,
-      {fields: ['place_id']}
-    );
-
-    originAutocomplete.addListener('place_changed', () => {
-      const place = originAutocomplete.getPlace();
-      this.originPlace = place.place_id;
-    });
-
-    finalAutocomplete.addListener('place_changed', () => {
-      const place = finalAutocomplete.getPlace();
-      this.finalPlace = place.place_id;
-      this.route();
-      this.setInfoWindow();
-    });
-
-    this.changeOfTravelMode(
-      "changemode-walking",
-      google.maps.TravelMode.WALKING,
-    );
-    this.changeOfTravelMode(
-      "changemode-transit",
-      google.maps.TravelMode.TRANSIT,
-    );
-    this.changeOfTravelMode(
-      "changemode-driving",
-      google.maps.TravelMode.DRIVING,
-    );
-  };
-
-  changeOfTravelMode(id, mode) {
-    document.getElementById(id).addEventListener("click", () => {
-      this.travelMode = mode;
-      this.route();
-      this.setInfoWindow();
-    });
-
-    routeButton.on('click', this.clearRouteOutput)
-  };
-
-  route() {
-    if (!this.originPlace || !this.finalPlace) {
-      window.alert("Fill in the place you want to find.");
-      return document.getElementById("final").value='';
+  //Functional methods for search of routes
+  const route = () => {
+    showLoading();
+    if (!originPlace || !finalPlace) {
+      window.swal('Fill in the place you want to find.');
+      return $('#final')[0].value='';
     };
   
-    this.directionsService.route(
+    directionsService.route(
       {
-        origin: { placeId: this.originPlace },
-        destination: { placeId: this.finalPlace },
-        travelMode: this.travelMode,
+        origin: { placeId: originPlace },
+        destination: { placeId: finalPlace },
+        waypoints: waypointPlace, 
+        travelMode: travelMode,
       },
       (response, status) => {
-        if (status === "OK") {
-          this.directionsRenderer.setDirections(response);
-          this.setInfoWindow(response);
+        if (status === 'OK') {
+          directionsRenderer.setMap(map);
+          directionsRenderer.setDirections(response);
+          setInfoWindowRoutes(response);
+          hideLoading();
         } else {
-          window.alert("Directions request failed due to " + status);
+          window.swal('Directions request failed due to ' + status, '', 'error');
         };
       }
     );
+  }  
+
+  const changeOfTravelMode = (id, mode) => {
+    $(`#${id}`).on('click', () => {
+      showLoading();
+      travelMode = mode;
+      route();
+    });
   };
 
-  clearRouteOutput() {
-    document.getElementById("origin").value='';
-    document.getElementById("final").value='';
-    this.directionsRenderer.setMap(null);
+  const clearRoute = () => {
+    originInput.value = '';
+    destinationInput.value = '';
+    $('#waypoint-wrapper').remove();
+    originPlace = '';
+    finalPlace = '';
+    waypointPlace.length = 0;
+    $('#waypoint-button').show();
+    directionsRenderer.setMap(null);
+    infoWindow.setPosition(null);
   }
 
-  setInfoWindow(response) {
+  const setInfoWindowRoutes = (response) => {
     const infoContent = `<p>
     Time of travel: ${response.routes[0].legs[0].distance.text}<br/>
     Distance: ${response.routes[0].legs[0].duration.text}
     </p>`;
-    this.infoWindow.setContent(infoContent);
+    infoWindow.setContent(infoContent);
 
     const step = Math.floor(response.routes[0].legs[0].steps.length / 2);
   
-    this.infoWindow.setPosition(response.routes[0].legs[0].steps[step].end_location);
-    this.infoWindow.open(map);
-
-    this.directionsRenderer.addListener('click', () => {
-      this.infoWindow.open(map);
-    })
+    infoWindow.setPosition(response.routes[0].legs[0].steps[step].end_location);
+    infoWindow.open(map);
   }
+
+  const addWaypoint = () => {
+    const waypoint = $(`<div id='waypoint-wrapper' class='waypoint-wrapper'>
+                          <input id='waypoint-input' class='waypoint-input' type='text'>
+                          <button id='clear-waypoint' class='clear-waypoint'>X</button>
+                        </div>`)
+      $('#route-search').append(waypoint);
+      const waypointAutocomplete = new google.maps.places.Autocomplete(
+        $('#waypoint-input')[0],
+        {fields: ['place_id']}
+      );
+      waypointAutocomplete.addListener('place_changed', () => {
+        const place = waypointAutocomplete.getPlace();
+        if (waypointPlace.length === 1) {
+          window.swal('Sorry. We currently support only one waypoint.')
+        } else {
+          waypointPlace.push({
+          location: { placeId: place.place_id },
+          stopover: true,
+        });}
+        route();
+      });
+      $('#waypoint-button').hide();
+      $('#clear-waypoint').on('click', clearWaypoint);
+  }
+
+  const clearWaypoint = () => {
+    $('#waypoint-input')[0].value = '';
+    waypointPlace.length= 0;
+    console.log('asas');
+    route();
+  }
+
+  //Change of travel option
+  changeOfTravelMode(
+    'changemode-walking',
+    google.maps.TravelMode.WALKING,
+  );
+  changeOfTravelMode(
+    'changemode-transit',
+    google.maps.TravelMode.TRANSIT,
+  );
+  changeOfTravelMode(
+    'changemode-driving',
+    google.maps.TravelMode.DRIVING,
+  );
+
+  //Event handlers
+  $('#geolocation-button').on('click', geolocation);
+  $('#place-button').on('click', clearPlace);
+  $('#route-button').on('click', clearRoute);
+  $('#waypoint-button').on('click', addWaypoint);
+  
 };
