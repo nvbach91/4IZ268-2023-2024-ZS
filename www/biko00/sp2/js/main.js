484cf5c331cc35063109a18e1ec7a5b40ca11a01
Oskar <56304441+OSway@users.noreply.github.com>
2024-01-26 01:40:06 +0100

    Version 03
    Fixing of mistakes

--- www/biko00/sp2/js/main.js
+++ www/biko00/sp2/js/main.js
@@ -1,30 +1,76 @@
-$(document).ready(function () {
-  $('#search-recipes').click(function () {
+$(document).ready(() => {
+  const recipeContainer = $('#recipe-container');
+  const searchRecipesButton = $('#search-recipes');
+  const modalLikedRecipesContainer = $('#modal-liked-recipes');
 
-    var cuisine = $('#cuisine').val();
-    var diet = $('#diet').val();
-    var intolerances = $('#intolerances').val();
-    var maxReadyTime = $('#maxReadyTime').val() || '30';
-    $('#recipe-container').html('');
+  // Cuisines select creation
+  const cuisines = ["African", "Asian", "American", "British", "Cajun", "Caribbean",
+    "Chinese", "Eastern European", "European", "French", "German",
+    "Greek", "Indian", "Irish", "Italian", "Japanese", "Jewish",
+    "Korean", "Latin American", "Mediterranean", "Mexican",
+    "Middle Eastern", "Nordic", "Southern", "Spanish", "Thai",
+    "Vietnamese"];
+  const selectCuisine = $('<select name="cuisine" id="cuisine"></select>');
+  selectCuisine.append($('<option value="Any"></option>'));
+  cuisines.forEach(cuisine => {
+    selectCuisine.append($(`<option value="${cuisine}">${cuisine}</option>`));
+  });
+  $('label[for="cuisine"]').after(selectCuisine);
+
+  // Diets select creation
+  const diets = ["Vegetarian", "Vegan", "Gluten Free", "Ketogenic"];
+  const selectDiet = $('<select name="diet" id="diet"></select>');
+  selectDiet.append($('<option value="Any"></option>'));
+  diets.forEach(diet => {
+    selectDiet.append($(`<option value="${diet}">${diet}</option>`));
+  });
+  $('label[for="diet"]').after(selectDiet);
+
+  // Intolerances select creation
+  const intolerances = ["Dairy", "Egg", "Gluten"];
+  const selectIntolerance = $('<select name="intolerances" id="intolerances"></select>');
+  selectIntolerance.append($('<option value="Any"></option>'));
+  intolerances.forEach(intolerance => {
+    selectIntolerance.append($(`<option value="${intolerance}">${intolerance}</option>`));
+  });
+  $('label[for="intolerances"]').after(selectIntolerance);
+
+  $('#show-liked-recipes').click(() => {
+    $('#liked-recipes-modal').show();
+    displayLikedRecipes();
+  });
+
+  // Close modal
+  $('.close-modal').click(() => {
+    $('#liked-recipes-modal').hide();
+  });
+
+  searchRecipesButton.click(() => {
+    const cuisine = $('#cuisine').val();
+    const diet = $('#diet').val();
+    const intolerances = $('#intolerances').val();
+    let amount = $('#amountRecipes').val() || '1';
+    let maxReadyTime = $('#maxReadyTime').val() || '30';
+    recipeContainer.html('');
     toggleVisibility('recipe-spinner', true);
     $.ajax({
       url: 'https://api.spoonacular.com/recipes/complexSearch',
       type: 'GET',
       dataType: 'json',
       data: {
-        apiKey: 'aa8b5e5027e04fac90bd86cbd40ef03a',
+        apiKey: '2503b80f31f046ea9b0ffb32a21f74ee',
         cuisine: cuisine,
         diet: diet,
         intolerances: intolerances,
         maxReadyTime: maxReadyTime,
         addRecipeInformation: true,
-        number: 3
+        number: amount
       },
-      success: function (response) {
+      success: (response) => {
         displayRecipes(response.results);
         toggleVisibility('recipe-spinner', false);
       },
-      error: function (error) {
+      error: (error) => {
         console.log('Error:', error);
         toggleVisibility('recipe-spinner', false);
       }
@@ -32,33 +78,21 @@ $(document).ready(function () {
 
   });
 
-  $('#show-liked-recipes').click(function () {
-    var likedRecipesContainer = $('#liked-recipes-container');
-    if (likedRecipesContainer.is(':visible')) {
-      likedRecipesContainer.hide();
-      $(this).text('Show Liked Recipes');
-    } else {
-      displayLikedRecipes();
-      likedRecipesContainer.show();
-      $(this).text('Hide Liked Recipes');
-    }
-  });
-
-  function displayRecipes(recipes) {
-    var recipeContainer = document.getElementById('recipe-container');
+  const displayRecipes = (recipes) => {
     recipeContainer.innerHTML = '';
-    let likedRecipes = JSON.parse(localStorage.getItem('likedRecipes')) || [];
+    const likedRecipes = JSON.parse(localStorage.getItem('likedRecipes')) || [];
+    const fragment = document.createDocumentFragment();
 
-    recipes.forEach(function (recipe) {
-      var template = document.getElementById('recipe-template');
-      var clone = template.cloneNode(true);
+    recipes.forEach((recipe) => {
+      const template = document.getElementById('recipe-template');
+      const clone = template.cloneNode(true);
 
       clone.style.display = 'block';
       clone.id = '';
 
       clone.querySelector('.recipe-title').textContent = recipe.title;
-      var likeButton = clone.querySelector('.like-button');
-      likeButton.onclick = function () { toggleLikeRecipe(recipe.id, likeButton); };
+      const likeButton = clone.querySelector('.like-button');
+      likeButton.onclick = () => { toggleLikeRecipe(recipe.id, likeButton); };
       likeButton.textContent = likedRecipes.includes(recipe.id) ? 'Unlike' : 'Like';
 
       clone.querySelector('.recipe-image').src = recipe.image;
@@ -67,20 +101,22 @@ $(document).ready(function () {
       displayIngredientTable(recipe, clone);
       displayInstructions(recipe, clone);
 
-      recipeContainer.appendChild(clone);
+      fragment.appendChild(clone);
     });
+    recipeContainer.append(fragment);
   }
 
-  function displayLikedRecipes() {
-    var likedRecipes = JSON.parse(localStorage.getItem('likedRecipes')) || [];
-    var likedRecipesContainer = document.getElementById('liked-recipes');
-    likedRecipesContainer.innerHTML = '';
+  const displayLikedRecipes = () => {
+    const likedRecipes = JSON.parse(localStorage.getItem('likedRecipes')) || [];
+    modalLikedRecipesContainer.empty();
 
     if (likedRecipes.length === 0) {
-      likedRecipesContainer.textContent = 'No liked recipes yet.';
+      console.log(likedRecipes.length)
+
+      modalLikedRecipesContainer.text('No liked recipes yet.');
     } else {
       toggleVisibility('liked-spinner', true);
-      likedRecipes.forEach(function (recipeId) {
+      likedRecipes.forEach((recipeId) => {
         fetchAndDisplayLikedRecipe(recipeId);
       });
     }
@@ -91,69 +127,51 @@ $(document).ready(function () {
       url: 'https://api.spoonacular.com/recipes/' + recipeId + '/information',
       type: 'GET',
       dataType: 'json',
-      data: { apiKey: 'aa8b5e5027e04fac90bd86cbd40ef03a' },
-      success: function (recipe) {
+      data: { apiKey: '2503b80f31f046ea9b0ffb32a21f74ee' },
+      success: (recipe) => {
         appendLikedRecipe(recipe);
         toggleVisibility('liked-spinner', false);
       },
-      error: function (error) {
+      error: (error) => {
         console.log('Error:', error);
         toggleVisibility('liked-spinner', false);
       }
     });
   }
 
-  function appendLikedRecipe(recipe) {
-    var likedRecipesContainer = document.getElementById('liked-recipes-container');
+  const appendLikedRecipe = (recipe) => {
+    const recipeContainer = $('<div class="recipe-item"></div>');
 
-    var recipeContainer = document.createElement('div');
-    recipeContainer.className = 'recipe-item';
-
-    var title = document.createElement('h1');
-    title.className = 'recipe-title';
-    title.textContent = recipe.title;
-
-    var image = document.createElement('img');
+    const title = $('<h1 class="recipe-title"></h1>').text(recipe.title);
+    const image = document.createElement('img');
     image.className = 'recipe-image';
     image.src = recipe.image;
     image.alt = recipe.title;
-
-    var unlikeButton = document.createElement('button');
+    const unlikeButton = document.createElement('button');
     unlikeButton.className = "btn btn-success like-button";
     unlikeButton.textContent = 'Unlike';
-    unlikeButton.addEventListener('click', function () {
+    unlikeButton.addEventListener('click', () => {
       removeRecipeFromLiked(recipe.id, recipeContainer);
     });
+    const instructionsContainer = $('<div class="recipe-instructions"></div>');
+    instructionsContainer.append('<h2>Instructions:</h2>');
+    const ol = $('<ol></ol>');
 
-    var instructions = document.createElement('div');
-    instructions.className = 'recipe-instructions';
-    var instructionsHeader = document.createElement('h2');
-    instructionsHeader.textContent = 'Instructions:';
-    instructions.appendChild(instructionsHeader);
-
-    var ol = document.createElement('ol');
     if (recipe.analyzedInstructions.length > 0 && recipe.analyzedInstructions[0].steps.length > 0) {
-      recipe.analyzedInstructions[0].steps.forEach(function (step) {
-        var li = document.createElement('li');
-        li.textContent = step.step;
-        ol.appendChild(li);
+      recipe.analyzedInstructions[0].steps.forEach((step) => {
+        ol.append(`<li>${step.step}</li>`);
       });
     } else {
-      var noInstructionsText = document.createElement('p');
-      noInstructionsText.textContent = 'Instructions not available.';
-      ol.appendChild(noInstructionsText);
+      ol.append('<p>Instructions not available.</p>');
     }
-    instructions.appendChild(ol);
 
-    recipeContainer.appendChild(title);
-    recipeContainer.appendChild(image);
-    recipeContainer.appendChild(unlikeButton);
-    recipeContainer.appendChild(instructions);
+    instructionsContainer.append(ol);
+    recipeContainer.append(title, image, unlikeButton, instructionsContainer);
+    $('#modal-liked-recipes').append(recipeContainer);
+  };
 
-    likedRecipesContainer.appendChild(recipeContainer);
-  }
 
-  function toggleLikeRecipe(recipeId, button) {
+  const toggleLikeRecipe = (recipeId, button) => {
     let likedRecipes = JSON.parse(localStorage.getItem('likedRecipes')) || [];
 
     if (likedRecipes.includes(recipeId)) {
@@ -166,7 +184,7 @@ $(document).ready(function () {
     localStorage.setItem('likedRecipes', JSON.stringify(likedRecipes));
   }
 
-  function removeRecipeFromLiked(recipeId, recipeElement) {
+  const removeRecipeFromLiked = (recipeId, recipeElement) => {
     let likedRecipes = JSON.parse(localStorage.getItem('likedRecipes')) || [];
     likedRecipes = likedRecipes.filter(id => id !== recipeId);
     localStorage.setItem('likedRecipes', JSON.stringify(likedRecipes));
@@ -174,44 +192,44 @@ $(document).ready(function () {
     recipeElement.remove();
   }
 
-  function displayIngredientTable(recipe, recipeEl) {
-    var ingredientsList = recipeEl.querySelector('.ingredients-list');
-    ingredientsList.innerHTML = '';
+  const displayIngredientTable = (recipe, recipeEl) => {
+    const ingredientsList = recipeEl.querySelector('.ingredients-list');
+    let html = '';
 
-    var allIngredients = [];
+    const allIngredients = [];
     if (recipe.analyzedInstructions && recipe.analyzedInstructions.length > 0 && recipe.analyzedInstructions[0].steps) {
-      recipe.analyzedInstructions[0].steps.forEach(function (step) {
-        step.ingredients.forEach(function (ingredient) {
-          if (allIngredients.indexOf(ingredient.name) === -1) {
+      recipe.analyzedInstructions[0].steps.forEach((step) => {
+        step.ingredients.forEach((ingredient) => {
+          if (!allIngredients.includes(ingredient.name)) {
             allIngredients.push(ingredient.name);
-            var li = document.createElement('li');
-            li.textContent = ingredient.name;
-            ingredientsList.appendChild(li);
+            html += `<li>${ingredient.name}</li>`;
           }
         });
       });
     } else {
-      ingredientsList.innerHTML = '<li>Ingredients not available.</li>';
+      html = '<li>Ingredients not available.</li>';
     }
+
+    ingredientsList.innerHTML = html;
   }
 
   function displayInstructions(recipe, recipeEl) {
-    var instructionsList = recipeEl.querySelector('.recipe-instructions ol');
-    instructionsList.innerHTML = '';
+    let instructionsList = recipeEl.querySelector('.recipe-instructions ol');
+    let html = '';
 
     if (recipe.analyzedInstructions && recipe.analyzedInstructions.length > 0 && recipe.analyzedInstructions[0].steps) {
-      recipe.analyzedInstructions[0].steps.forEach(function (step) {
-        var li = document.createElement('li');
-        li.textContent = step.step;
-        instructionsList.appendChild(li);
+      recipe.analyzedInstructions[0].steps.forEach((step) => {
+        html += `<li>${step.step}</li>`;
       });
     } else {
-      instructionsList.innerHTML = '<li>Instructions not available.</li>';
+      html = '<li>Instructions not available.</li>';
     }
+
+    instructionsList.innerHTML = html;
   }
 
-  function toggleVisibility(elementId, isVisible) {
-    var element = document.getElementById(elementId);
+  const toggleVisibility = (elementId, isVisible) => {
+    const element = document.getElementById(elementId);
     if (element) {
       if (isVisible) {
         element.classList.remove('hidden');
